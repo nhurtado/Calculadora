fed22d07eedf58b2482d56cc8c919e5cfd252123
Nhurtado <nhurtado@miuandes.cl>
2018-09-09 02:40:20 -0300

    update.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @ufs = DateValue.where('extract(year from date_v) = ?', 1984).to_json
+    @ufs = DateValue.where('extract(year from date_v) = ?', 1984).where('extract(month from date_v) = ?', 10).to_json
   end
 end
